1d545ea7bfb9383433b0f8f57ed43000c7bb0533
sat-ess <84312123+sat-ess@users.noreply.github.com>
2021-09-24 22:53:15 +0530

    Add files via upload

---
+++ SEQUENCES/PS_TXN_SEQ.sql
@@ -0,0 +1,5 @@
+--------------------------------------------------------
+--  DDL for Sequence PS_TXN_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SLS"."PS_TXN_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 50 START WITH 819451 CACHE 20 NOORDER  NOCYCLE   ;
---
+++ SEQUENCES/SEQ_SLS_SUBC_PACKAGE.sql
@@ -0,0 +1,5 @@
+--------------------------------------------------------
+--  DDL for Sequence SEQ_SLS_SUBC_PACKAGE
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SLS"."SEQ_SLS_SUBC_PACKAGE"  MINVALUE 1 MAXVALUE 999999999999999999999999999 INCREMENT BY 1 START WITH 1 NOCACHE  NOORDER  NOCYCLE   ;
