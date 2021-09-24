d334f631b3699ed0eed2c1d267eab7d48162022c
sat-ess <84312123+sat-ess@users.noreply.github.com>
2021-09-24 22:53:33 +0530

    Add files via upload

---
+++ SYNONYMS/APP_EE_PRF_SUB_TYPE.sql
@@ -0,0 +1,5 @@
+--------------------------------------------------------
+--  DDL for Synonymn APP$EE$PRF$SUB$TYPE
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "SLS"."APP$EE$PRF$SUB$TYPE" FOR "APP"."APP$EE$PRF$SUB$TYPE";
---
+++ SYNONYMS/APP_UOM_CONV_STD.sql
@@ -0,0 +1,5 @@
+--------------------------------------------------------
+--  DDL for Synonymn APP$UOM$CONV$STD
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "SLS"."APP$UOM$CONV$STD" FOR "APP"."APP$UOM$CONV$STD";
